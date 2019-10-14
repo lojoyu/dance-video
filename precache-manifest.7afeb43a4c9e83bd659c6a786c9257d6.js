15feee399e11767c2e46a120ea83b4ba543c8124
lojoyu <sophie200429@gmail.com>
2019-10-14 12:57:01 +0800

    Updates

---
+++ precache-manifest.7afeb43a4c9e83bd659c6a786c9257d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d3df3a4921705579d34c14032074c1",
+    "url": "/dance-video/index.html"
+  },
+  {
+    "revision": "4021c797337e224b54f0",
+    "url": "/dance-video/static/css/main.75e2e6ee.chunk.css"
+  },
+  {
+    "revision": "3f275ff050e5ccd4e783",
+    "url": "/dance-video/static/js/2.19f926e7.chunk.js"
+  },
+  {
+    "revision": "4021c797337e224b54f0",
+    "url": "/dance-video/static/js/main.ce798c01.chunk.js"
+  },
+  {
+    "revision": "72295676cd60cba67465",
+    "url": "/dance-video/static/js/runtime-main.6fc58fa9.js"
+  }
+]);
